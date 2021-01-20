de3b7dbcb477ba9864872d476d447c2d5a4f7792
Rifqie Akma <rifqieakma@gmail.com>
2021-01-20 09:21:21 +0800

    Simple Inventory

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import StockEventsTable from './components/StockEventsTable'
+
+class App extends React.Component{
+  state = {
+    products: [],
+    stockEvents: [],
+  }
+
+  async componentDidMount(){
+
+    const productsRes = await axios({
+      method: 'GET',
+      url: 'http://localhost:1337/products'
+    })
+
+    const stockEventsRes = await axios({
+      method: 'GET',
+      url: 'http://localhost:1337/stockevents'
+    })
+    console.log("App.componentDidMount stockEventsRes", stockEventsRes)
+
+    const products = productsRes.data
+    const stockEvents = stockEventsRes.data
+
+    this.setState({products, stockEvents})
+  }
+
+  render(){
+    console.log("App.render")
+    const {products, stockEvents} = this.state
+
+    return (
+      <div className="App">
+        <h1>The Stock App</h1>
+
+        <StockEventsTable
+          products={products}
+          stockEvents={stockEvents}
+        />
+
+      </div>
+    );
+  }
 }
 
 export default App;
